77ed7eefa578f13a7db8ca033948c2a4a39143f6
sebastian.kuebeck <sebastian.kuebeck@vitavo.at>
2026-07-14 13:43:10 +0200

    Updated dependencies for examples

--- examples/ci-cd/scan.sh
+++ examples/ci-cd/scan.sh
@@ -3,6 +3,7 @@
 set -e
 
 export PATH="$VIRTUAL_ENV/bin:$PATH"
+cutoff="$(date -u -d '7 days ago' +'%Y-%m-%dT%H:%M:%SZ')"
 
 #
 # Generates an SBOM from the docker image
@@ -13,7 +14,7 @@ debsbom --progress generate -t cdx -o /var/shared/docker-image.sbom.cdx.json
 #
 # Scans requirements file and generates a "locked" version with hashes
 #
-pipcanary -p /var/shared/pyproject.toml -l /var/shared/requirements-locked.txt --allow-upload-time='urllib3<=2026-05-07T16:13:18'
+pipcanary -p /var/shared/pyproject.toml -l /var/shared/requirements-locked.txt --pip-args="--uploaded-prior-to=$cutoff"
 
 #
 # Generates an SBOM from the locked version requirements file
